3bb13686538ec296f93af805362d086781efb89e
Oliver Benns <oliverbenns@gmail.com>
2018-02-16 10:10:04 -0500

    Migrate entry code to v3

--- src/index.js
+++ src/index.js
@@ -4,32 +4,32 @@ import pkg from '../package.json';
 
 // This is the entry point of your game.
 
+const width = 800;
+const height = 600;
+
 const config = {
-  width: 800,
-  height: 600,
-  renderer: Phaser.AUTO,
-  parent: '',
-  state: {
-    preload,
-    create,
-  },
+  width,
+  height,
+  type: Phaser.AUTO,
   transparent: false,
   antialias: true,
-  physicsConfig: { arcade: true },
+  scene: { preload, create },
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.game.load.image('study', 'assets/img/study.png');
+  this.load.image('study', 'assets/img/study.png');
 }
 
 function create() {
-  const { game } = this;
-  const objects = [
-    game.add.text(game.world.centerX, game.world.centerY * 0.8, `Welcome to Phaser ${pkg.dependencies.phaser.substr(1)}`, { font: "bold 19px Arial", fill: "#fff" }),
-    game.add.sprite(game.world.centerX, game.world.centerY * 1.2, 'study')
-  ];
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const welcomeMessage = `Welcome to Phaser ${pkg.dependencies.phaser.substr(1)}`;
 
-  objects.forEach(obj => obj.anchor.setTo(0.5, 0.5));
+  this.add.image(centerX, centerY * 1.2, 'study');
+
+  this.add
+    .text(centerX, centerY * 0.8, welcomeMessage, { font: "bold 19px Arial", fill: "#fff" })
+    .setOrigin(0.5, 0.5);
 }
